358c84cc174f9cecfd9aa9d3376ccfc9a9bb2e3d
Don Kasun Gallage <kasungallage94@gmail.com>
2026-01-24 19:23:45 +0530

    Implements basic UI and device management
    Initializes the basic UI structure and integrates device management using Zustand.
    
    Adds UI components and navigation. Introduces a device store to manage device states and interactions.
    Configures the app for a dark user interface style and includes Inter fonts for improved aesthetics.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,19 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold 
+} from '@expo-google-fonts/inter';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +21,18 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [loaded, error] = useFonts({
+    'Inter-Regular': Inter_400Regular,
+    'Inter-Bold': Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
